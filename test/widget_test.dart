26a6118de7ef16114f25d068481fdb9c819749ba
AustinLynes <austinlynes94@gmail.com>
2020-03-17 08:31:55 -0700

    fixing tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,10 +11,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:chatta/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Checking if hello world shows up', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
-
-      expect(find.text('Hello World!'), findsOneWidget);
+    expect(find.text('Hello World!'), findsOneWidget);
   });
 }
